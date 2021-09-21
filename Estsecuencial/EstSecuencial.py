cb6cd98df0204324d8e9f6f29c3a21ce30164ce6
AlvaroXD12 <>
2021-09-21 13:58:58 +0000

    Calculando promedio final

---
+++ Estsecuencial/EstSecuencial.py
@@ -0,0 +1,8 @@
+#Datos de Entrada
+print("Hallar el area del rombo")
+DM=int(input("Ingrese diagonal Mayor:"))
+dm=int(input("Ingrese diagonal menor:"))
+#Proceso
+area=(DM*dm)/2
+#Datos de salida
+print("El area del rombo es:" , area , "mt2")
