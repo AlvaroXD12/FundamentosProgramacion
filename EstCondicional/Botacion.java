173a7d4d41be1a9816e03c2b8876d147ee951962
AlvaroXD12 <>
2021-09-29 16:24:38 +0000

    Trabajo de botaciones

---
+++ EstCondicional/Botacion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Botacion {
+  static Scanner entrada= new Scanner(System.in);
+  public static void main(String[]args){
+    //Declaracion de variable e iniciacion
+    int edad=0;
+    //Datos de entrada
+     System.out.println("Ingrese su edad:");
+     edad=entrada.nextInt();
+     //Proceso
+     if(edad>=18){
+       System.out.println("Usted si puede votar por tener:" + edad);
+     }else{
+     if(edad<18){
+       System.out.println("Usted no puede votar por tener:" + edad);
+     }
+       
+     }
+    }
+}
