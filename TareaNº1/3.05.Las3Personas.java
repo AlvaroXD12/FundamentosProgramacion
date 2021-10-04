70b2aa9284d6415fa06f2bcd382ebd578daf6079
AlvaroXD12 <>
2021-10-04 16:20:46 +0000

    tarea

--- TareaNº1/3.05.Las3Personas.java
+++ TareaNº1/3.05.Las3Personas.java
@@ -3,19 +3,35 @@ public class Las3Personas {
   static Scanner entrada = new Scanner(System.in);
   public static void main(String[]args){
     //Declaracion de variable
-    int mayor=30; int medio=25; int menor=20; int cod=0;
+    int edadA=0, edadB=0, edadC=0, eDad=0;
+    String nombre1, nombre2, nombre3, mEnor="";
     //Datos de entrada
-    System.out.println("ingresar su codigo:");
-    cod=entrada.nextInt();
+    System.out.println("ingresar su nombre:");
+    nombre1=entrada.next();
+    System.out.println("ingresar su edad:");
+    edadA=entrada.nextInt();
+    System.out.println("ingresar su nombre:");
+    nombre2=entrada.next();
+    System.out.println("ingresar su edad:");
+    edadB=entrada.nextInt();
+    System.out.println("ingresar su nombre:");
+    nombre3=entrada.next();
+    System.out.println("ingresar su edad:");
+    edadC=entrada.nextInt();
     //Datos de salida
-    if (cod==2504){
-      System.out.println("Su nombre es Juan y su edad es:" +menor);
+    if(edadA<edadB && edadA<edadC){
+    eDad=edadA;
+    mEnor=nombre1;
     }else{
-    if (cod==2505){
-      System.out.println("Su nombre es Pepe y su edad es:"+medio);
+    if(edadB<edadA && edadB<edadC){
+    eDad=edadB;
+    mEnor=nombre2;
     }else{
-    if (cod==2506){  
-      System.out.println("Su nombre es Francisco y su edad es:" +mayor);
+    if(edadC<edadA && edadC<edadB){
+    eDad=edadC;
+    mEnor=nombre3;
     }}}
+    //Datos de salida
+    System.out.println("La persona menor es: "+ mEnor +" y su edad es:" +eDad);
   }
 }
