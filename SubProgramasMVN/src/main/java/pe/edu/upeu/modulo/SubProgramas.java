0dff31692da000b404ad01e44171b0a32010fac0
AlvaroXD12 <>
2021-11-09 13:31:25 +0000

    Subprogramas mvn

--- SubProgramasMVN/src/main/java/pe/edu/upeu/modulo/SubProgramas.java
+++ SubProgramasMVN/src/main/java/pe/edu/upeu/modulo/SubProgramas.java
@@ -2,17 +2,17 @@ package pe.edu.upeu.modulo;
 
 public class SubProgramas{
 
-  public String nombre:"Alvaro";
-  public String dni:"";
+  public String nombre="David";
+  public String dni=""; 
 
   public void saludo(){
-    System.out.println("hola"+nombre);
+    System.out.println("Hola "+nombre);
   }
 
   public void otroSaludo(String nombrex){
     this.nombre=nombrex;
-    System.out.println("hola"+nombre);
+    System.out.println("Hola "+nombre);
   }
 
-  
+
 }
