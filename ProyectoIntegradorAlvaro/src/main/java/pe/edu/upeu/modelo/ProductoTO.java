583a118f33bc8337ece299b734864edd6ed26113
AlvaroXD12 <alvaro.maguina@upeu.edu.pe>
2021-12-06 01:45:39 -0500

    ProyectoIntegrador

--- ProyectoIntegradorAlvaro/src/main/java/pe/edu/upeu/modelo/ProductoTO.java
+++ ProyectoIntegradorAlvaro/src/main/java/pe/edu/upeu/modelo/ProductoTO.java
@@ -1,5 +1,10 @@
 package pe.edu.upeu.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+
 public class ProductoTO {
     public String idProd;
     public String nombre;
---
+++ ProyectoIntegradorAlvaro/src/main/java/pe/edu/upeu/modelo/UsuarioTO.java
@@ -0,0 +1,11 @@
+package pe.edu.upeu.modelo;
+
+import lombok.Data;
+
+@Data
+public class UsuarioTO {
+    public String usuario;
+    public String password;
+    public String perfil;
+
+}
